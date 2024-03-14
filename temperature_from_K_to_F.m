8b18f7ac23f3ae50caa8819c9b047648bdf5bce4
soheil <140661028+soheilsolim@users.noreply.github.com>
2024-03-14 11:04:32 -0700

    Add files via upload

---
+++ temperature_from_K_to_F.m
@@ -0,0 +1,7 @@
+%soheil mansouri (electrical engineering)
+%script file to convert temperature from K to F
+%
+K=input('temperature in degrees K:');  %give K
+F=((K*9/5)-459.67);
+disp(['Temperature in degrees F=',num2str(F)])  %get F
+%stop
